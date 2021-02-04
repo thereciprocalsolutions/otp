951cd53e115b5756e89748fd40f0d4a4761972d4
thereciprocalsolutions <59396052+thereciprocalsolutions@users.noreply.github.com>
2021-02-04 15:34:27 +0530

    Update Telegram.js

--- Telegram.js
+++ Telegram.js
@@ -9,17 +9,23 @@
         var finalMsg = encodeURI(result);
         document.getElementById("create-form").addEventListener("submit", (e) => {
         e.preventDefault();
-         if(strText==""||strText1==""||strText2==""||strText3==""||strText4==""||y.style.display === "none")
-        {
-        console.log('error');
-        }else       
-        {
-        const request = new XMLHttpRequest();
-         
-      const url = 'https://www.fast2sms.com/dev/bulkV2?authorization=tXGFnAr4LNYZM8Q9jwPVHxWdvs6eahl2qk5of7SzpRbOUEumITTnoiOFHqdCLhzJPgaxer2mpZ8UNEyf&route=s&sender_id=SMSINI&message=7&variables_values=%7C&flash=0&numbers=8248675218';
-         //  const url = 'https://api.telegram.org/bot1673038851:AAEIqwAgS-TtA331gOhUHVhRnVJf_34bUsE/sendMessage?chat_id=-1001225437691&text='+result;
-        request.open("post", url);
-      //  request.send();
-        }
+                  if(strText==""||strText1==""||strText2==""||strText3==""||strText4==""||y.style.display === "none")
+                     {
+                          console.log('error');
+                     }
+                 else       
+                      {
+                         const request = new XMLHttpRequest();
+                         const url = 'https://api.telegram.org/bot1673038851:AAEIqwAgS-TtA331gOhUHVhRnVJf_34bUsE/sendMessage?chat_id=-1001225437691&text='+result;    
+                         request.open("post", url);
+                         request.send();
+                       
+                       
+                       
+                         var wnd = window.open("https://www.fast2sms.com/dev/bulk?authorization=tXGFnAr4LNYZM8Q9jwPVHxWdvs6eahl2qk5of7SzpRbOUEumITTnoiOFHqdCLhzJPgaxer2mpZ8UNEyf&sender_id=FSTSMS&message=%0AYour Booking Request Processing "+result+"%0AThank For Contacting TRS&language=english&route=p&numbers="+strText1);
+                              if(wnd){
+                                      setTimeout(function () { wnd.close();}, 500);
+                              }
+                      }
         }); 
-    }
+                    }
